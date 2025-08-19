437d577fac608158de32b58f09262ad034f3a0d2
VedpathakMeghana <meghana.vedpathak@atos.net>
2025-08-19 15:42:59 +0530

    feat: Added Report SP and added new records in tblDataEntry and tblUser table

---
+++ db-scripts/99 Direct Scripts in Sequence/tblUser_AddRecord.sql
@@ -0,0 +1,4 @@
+
+Insert into [tblUser] (UserID,UserName,User_Loc,UserLevel) values ('Meghana.Vedpathak@defradev.onmicrosoft.com','Meghana Vedpathak','13',1)
+GO
+Insert into [tblUser] (UserID,UserName,User_Loc,UserLevel) values ('Edward.Fernandes@defradev.onmicrosoft.com','Edward Fernandes','13',1)
