f3137b50eb3c2be6bb76d5ba43473daf509ffd98
VedpathakMeghana <meghana.vedpathak@atos.net>
2025-09-02 12:41:00 +0000

    feat: formatting for error log message

--- src/Apha.BST/Apha.BST.Web/PresentationService/LogService.cs
+++ src/Apha.BST/Apha.BST.Web/PresentationService/LogService.cs
@@ -15,14 +15,14 @@
         {
             string errorCode= "500 - Internal Server Error";
             string generalErrorType = _configuration["ExceptionTypes:General"] ?? "BSTDefaultGeneralException";
-            _logger.LogError(ex, "[{ErrorType:l}] Error [{ErrorCode:l}] [{Context:l}]: {Message}", generalErrorType,errorCode ,context, ex.Message);
+            _logger.LogError(ex, "[{ErrorType:l}] Error [{ErrorCode:l} - {Context:l}]: {Message}", generalErrorType,errorCode ,context, ex.Message);
         }
 
         public void LogSqlException(Exception ex, string context)
         {
             string errorCode= "500 - SQL Server Error";
             string sqlErrorType = _configuration["ExceptionTypes:Sql"] ?? "BSTDefaultGeneralException";
-            _logger.LogError(ex, "[{ErrorType:l}] Error [{ErrorCode:l}] [{Context:l}]: {Message}", sqlErrorType,errorCode,context, ex.Message);
+            _logger.LogError(ex, "[{ErrorType:l}] Error [{ErrorCode:l} - {Context:l}]: {Message}", sqlErrorType,errorCode,context, ex.Message);
         }
     }
 }
